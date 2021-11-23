9d2f68ad6c283e3e2cd856bc5435cf80fb4757f2
내 이름 <you@example.com>
2021-11-23 14:36:55 +0900

    exceptino.py commit

---
+++ function01.py
@@ -0,0 +1,15 @@
+a = 10
+b = 20
+
+# c = a + b
+# d = a - b
+# e = a * b
+# f = a / b
+def fun(a,b):
+    c = a + b
+    d = a - b
+    e = a * b
+    f = a / b
+    return c,d,e,f
+
+print(fun(a,b))
